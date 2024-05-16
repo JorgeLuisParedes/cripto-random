3dd73d4f66ae615cac211ba5ccde70b049a28e8c
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-05-16 12:54:49 +0200

    Custom Hook - useRandom

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,8 @@
 import "./App.css";
-import { useQuery } from "@tanstack/react-query";
-
-const getRandomNumberFromApi = async (): Promise<number> => {
-  const res = await fetch(
-    "https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new"
-  );
-  const numberString = await res.text();
-  //   throw new Error("Auxilio!!");
-  return +numberString;
-};
+import { useRanDom } from "./hooks/useRandom";
 
 export const App = () => {
-  const query = useQuery({
-    queryKey: ["randomNumber"],
-    queryFn: getRandomNumberFromApi,
-  });
+  const query = useRanDom();
 
   return (
     <>
