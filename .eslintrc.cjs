e20ece2553345ba3b69aab150d25bd5184d8392c
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-05-30 20:06:42 +0200

    Se configuro Eslint y Prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,15 +5,21 @@ module.exports = {
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended',
 		'plugin:react-hooks/recommended',
+		'standard',
+		'plugin:prettier/recommended',
 	],
 	ignorePatterns: ['dist', '.eslintrc.cjs'],
 	parser: '@typescript-eslint/parser',
-	plugins: ['react-refresh'],
+	plugins: ['react-refresh', '@typescript-eslint'],
 	rules: {
 		'react-refresh/only-export-components': [
 			'warn',
 			{ allowConstantExport: true },
 		],
-		'@typescript-eslint/no-unused-vars': 'off'
+		'@typescript-eslint/no-unused-vars': 'off',
+		'import/export': 0,
+		'react/no-unescaped-entities': 'off',
+		'react-hooks/exhaustive-deps': 'off',
+		'no-unused-vars': 'off',
 	},
-}
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import "./App.css";
-import { useRanDom } from "./hooks/useRandom";
+import './App.css';
+import { useRanDom } from './hooks/useRandom';
 
 export const App = () => {
-  const query = useRanDom();
+	const query = useRanDom();
 
-  return (
-    <>
-      {query.isFetching ? (
-        <h2>Cargando...</h2>
-      ) : (
-        <h2>Número aleatorio: {query.data}</h2>
-      )}
+	return (
+		<>
+			{query.isFetching ? (
+				<h2>Cargando...</h2>
+			) : (
+				<h2>Número aleatorio: {query.data}</h2>
+			)}
 
-      {!query.isFetching && query.isError && <h3>{`${query.error}`}</h3>}
-      <button onClick={() => query.refetch()} disabled={query.isFetching}>
-        {query.isFetching ? "..." : "Nuevo número"}
-      </button>
-    </>
-  );
+			{!query.isFetching && query.isError && <h3>{`${query.error}`}</h3>}
+			<button onClick={() => query.refetch()} disabled={query.isFetching}>
+				{query.isFetching ? '...' : 'Nuevo número'}
+			</button>
+		</>
+	);
 };
