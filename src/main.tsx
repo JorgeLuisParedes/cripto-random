8db2d464991e139ef64477d6b1bfec254d7cee55
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-10-08 13:00:44 +0200

    TanStack - Query - Instalación

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+// Create a client
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   //   <StrictMode>
-  <App />
-  //   </StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <App />
+    <ReactQueryDevtools />
+  </QueryClientProvider>
+  // </StrictMode>,
 );
