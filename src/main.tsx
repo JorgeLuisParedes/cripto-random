7bf01ff1d50a062008cc41b0430cdc9ad492a0f1
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-10-08 14:20:33 +0200

    Manejo de errores

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,13 @@ import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 // Create a client
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  //   defaultOptions: {
+  //     queries: {
+  //       refetchOnWindowFocus: false,
+  //     },
+  //   },
+});
 
 createRoot(document.getElementById("root")!).render(
   //   <StrictMode>
